629f4bae8efcdae43b1f2e81159e02ec3ac0ca37
Simon Schab <simonmschab@gmail.com>
2025-06-01 22:41:21 +0200

    Set up Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="flex items-center justify-center min-h-screen bg-blue-50">
+        <h1 className="text-4xl font-bold text-blue-600">Tailwind CSS is working!</h1>
+      </div>
   );
 }
 
